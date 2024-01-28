9d9a901ec184f359d327c219ea4443fa727791f6
William Argiroff <williamargiroff@gmail.com>
2024-01-27 23:34:22 -0500

    Delete old qiime2 and manifest scripts

--- code/run_get_manifest_rscript.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Author: William Argiroff
-
-# Run R script to make manifest files for QIIME2
-# Inputs: R script (code/rscripts/get_manifest.R)
-# Outputs: manifest files for each set of fastq files
-
-Rscript code/rscripts/get_manifest.R
